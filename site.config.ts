9ce18724df70d8ce28f44f6d33126fc5f4020024
yu-song16 <104424505+yu-song16@users.noreply.github.com>
2022-04-26 17:20:23 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2819d60e4c054808958b193cf0c10834',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Yuniverse',
+  domain: 'https://mesquite-brook-0eb.notion.site/Yu-SONG-2819d60e4c054808958b193cf0c10834',
+  author: 'Yu Song',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'Yu79062305',
+  github: 'yu-song16',
+  linkedin: '钰-宋-662792227',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
